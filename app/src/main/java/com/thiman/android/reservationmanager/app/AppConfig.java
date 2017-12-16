d4845f8b310c953c61f39b73b3a3a82559d319a2
Thimankrishh <mysoftware15@gmail.com>
2017-12-16 09:53:51 +0530

    Initial commit

---
+++ app/src/main/java/com/thiman/android/reservationmanager/app/AppConfig.java
@@ -0,0 +1,15 @@
+package com.thiman.android.reservationmanager.app;
+
+/**
+ * Created by Asus on 12/9/2017.
+ */
+
+
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "https://long-drawn-collisio.000webhostapp.com/android_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "https://long-drawn-collisio.000webhostapp.com/android_api/register.php";
+}
